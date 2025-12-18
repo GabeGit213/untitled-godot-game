9cb81e6015570caf568aa559e9c8c4b5ca5c3eb4
gabe <salvarilie@gmail.com>
2025-12-18 21:57:40 +0200

    added new scene for house along with chest and npc, tweaked some stuff

--- scenes/global.gd
+++ scenes/global.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+var current_scene = "world"
+var transition_scene = false
+
+var player_exit_house_posx = 0
+var player_exit_house_posy = 0
+var player_start_posx = 0
+var player_start_posy = 0
