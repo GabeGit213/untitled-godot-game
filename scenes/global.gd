b47e133621bb5c14ce3e7b898966c1544c3cec0c
gabe <salvarilie@gmail.com>
2025-12-19 18:43:19 +0200

    i tried to make the scenes changeable but i failed. im giving up on that and instead making the houses abandones, and i will focus more on the main monk npc and the battle system that will initiate

--- scenes/global.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var current_scene = "world"
-var transition_scene = false
-
-var player_exit_house_posx = 0
-var player_exit_house_posy = 0
-var player_start_posx = 0
-var player_start_posy = 0
